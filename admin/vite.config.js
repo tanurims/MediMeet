33960f122e76a870f2ed33bcf121c899fe5a0629
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-06-19 22:22:12 +0530

    create setup for admin panel

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,11 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),    
+    react(),
     tailwindcss(),
-],
-
-  server: {
-    port: 5173
+  ],
+  server:{
+    port:5174
   }
 })
