d5f1aaedecdf2f510f2dbb2ddc21bbb609f5713f
tanurims <2022cs194@stu.ucsc.cmb.ac.lk>
2025-06-20 14:27:36 +0530

    create sidebar

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -11,6 +11,7 @@ import tick_icon from './tick_icon.svg'
 import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
+import logo from './logo1.svg'
 
 export const assets = {
     add_icon,
@@ -25,7 +26,8 @@ export const assets = {
     list_icon,
     tick_icon,
     appointments_icon,
-    earning_icon
+    earning_icon,
+    logo
 }
 
 export default assets
